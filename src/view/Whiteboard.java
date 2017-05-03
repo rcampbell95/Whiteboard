a749ce77c7c78fc6fc5d1b3c1e9de55d9360988b
Robert Campbell <robertgcampbell95@gmail.com>
2017-05-02 20:58:35 -0700

    Create empty project

---
+++ src/view/Whiteboard.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class Whiteboard extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
