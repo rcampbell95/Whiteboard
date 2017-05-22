23e30d3250a421a4efea21b1a4d9c6c30070cd3f
MarkFilchenko <markfilchenko@gmail.com>
2017-05-21 21:56:57 -0700

    TableModel comments added

--- src/model/TableModel.java
+++ src/model/TableModel.java
@@ -10,20 +10,32 @@ import view.DShape;
 public class TableModel extends AbstractTableModel implements ModelListener{
 
 	ArrayList<DShapeModel> modelArray;
-	public static final String [] COLUMN_HEADERS = { "X", "Y", "Width", "Height"};	
+	public static final String [] COLUMN_HEADERS = { "X", "Y", "Width", "Height"};
 
+	/**
+	 * TableModel Constructor
+	 * Makes an empty arraylist for the models
+	 */
 	public TableModel()
 	{
 		super();		
-		modelArray = new  ArrayList<>();
+		modelArray = new ArrayList<>();
 	}
-	
+
+	/**
+	 * Gets the column name
+	 * @param column the number of the column
+	 * @return column
+	 */
 	@Override
 	public String getColumnName(int column) {
 		return COLUMN_HEADERS[column];
 	}
-	
 
+	/**
+	 * Adds a model to the ArrayList that holds the models for the table, updates the table
+	 * @param model
+	 */
 	public void addModel(DShapeModel model)
 	{
 		modelArray.add(0, model);
@@ -32,6 +44,10 @@ public class TableModel extends AbstractTableModel implements ModelListener{
 
 	}
 
+	/**
+	 * Removes a model from the ArrayList that holds the models for the table, updates the table
+	 * @param model
+	 */
 	public void removeModel(DShapeModel model) {
         model.removeListener(this);
         modelArray.remove(model);
@@ -39,6 +55,9 @@ public class TableModel extends AbstractTableModel implements ModelListener{
 
     }
 
+	/**
+	 * Clears the table
+	 */
 	public void clearTable()
 	{
 		if(!(modelArray.isEmpty()))
@@ -48,15 +67,27 @@ public class TableModel extends AbstractTableModel implements ModelListener{
 		}
 	}
 
+	/**
+	 * Gets the row for a specific model
+	 * @param model the model that the row is to be found for
+	 * @return row
+	 */
 	public int getRowForModel(DShapeModel model) {
 	    return modelArray.indexOf(model);
     }
 
+	/**
+	 * Gets the column count (always gonna be 4 for this project)
+	 */
 	@Override
 	public int getColumnCount() {
 		return 4;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	@Override
 	public int getRowCount() {
 
@@ -84,6 +115,10 @@ public class TableModel extends AbstractTableModel implements ModelListener{
 		
 	}
 
+	/**
+	 * Implementation for the model changed interface, if the model is changed, the table is updated
+	 * @param model
+	 */
 	@Override
 	public void modelChanged(DShapeModel model) {
 
@@ -98,6 +133,10 @@ public class TableModel extends AbstractTableModel implements ModelListener{
 		}
 	}
 
+	/**
+	 * Moves a model to the beginning of the arraylist
+	 * @param model
+	 */
 	public void moveToFront(DShapeModel model)
 	{
 		if(!modelArray.isEmpty() && modelArray.contains(model))
@@ -109,6 +148,10 @@ public class TableModel extends AbstractTableModel implements ModelListener{
 		
 	}
 
+	/**
+	 * Moves a model to the end of the arraylist
+	 * @param model
+	 */
 	public void moveToBack(DShapeModel model)
 	{
 		if(!modelArray.isEmpty() && modelArray.contains(model))
@@ -118,6 +161,10 @@ public class TableModel extends AbstractTableModel implements ModelListener{
 			
 		}
 	}
+
+	/**
+	 * Sets the cells to not be editable in the table
+	 */
 	@Override
 	public boolean isCellEditable(int row, int column)
 	{
