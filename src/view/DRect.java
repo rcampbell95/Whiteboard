58f2b94af5052dabe85927d7cab5157b023208cd
Ruben Glapa <rubenglapa@gmail.com>
2017-05-21 21:57:20 -0700

    Comments for DOval, DRect, and DShape

--- src/view/DRect.java
+++ src/view/DRect.java
@@ -8,11 +8,21 @@ import model.DShapeModel;
 
 public class DRect extends DShape {
 
+	/**
+	 * Constructor
+	 * @param model
+	 * @param canvas
+	 */
 	public DRect(DShapeModel model, Canvas canvas) {
 		super(model, canvas);
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Draw a DRect
+	 * @param g
+	 * @param selected
+	 */
 	@Override
 	public void draw(Graphics g, boolean selected) {
 		//Rectangle shape = new Rectangle(model.getX(), model.getY(), model.getWidth(), model.getHeight());
@@ -24,6 +34,11 @@ public class DRect extends DShape {
 			drawKnobs(g);
 		}
 	}
+
+	/**
+	 * Return DRectModel
+	 * @return
+	 */
 	@Override
 	public DRectModel getModel() {
 		return (DRectModel) model;
